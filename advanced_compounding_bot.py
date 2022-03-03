8bde1127356f3a42bbfca77b630b57144b6d055a
JannikMottulla <38241107+JannikMottulla@users.noreply.github.com>
2022-03-03 16:23:25 +0100

    Update advanced_compounding_bot.py

--- advanced_compounding_bot.py
+++ advanced_compounding_bot.py
@@ -18,11 +18,11 @@ class AdvancedCompoundingBot:
             pyautogui.click(compound_button_location_center.x,
                             compound_button_location_center.y)
             time.sleep(2)
-            pyautogui.click(1266, 200)
+            pyautogui.click(2855, 865)
             time.sleep(1)
-            pyautogui.scroll(-80)
+            pyautogui.scroll(-200)
             time.sleep(1)
-            pyautogui.click(1266, 567)
+            pyautogui.click(2944, 1120)
             time.sleep(20)
             compound_success = self.check_if_compound_succes()
             if compound_success == True:
